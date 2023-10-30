cbdc87e3ba23754ebc4f546f30d0e444f06f003e
ukking1250 <ukking1250@gmail.com>
2023-10-30 19:29:27 +0900

    5

---
+++ lib/test/first_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class FirstTestView extends StatefulWidget {
+  const FirstTestView({super.key});
+
+  @override
+  State<FirstTestView> createState() => _FirstTestViewState();
+}
+
+class _FirstTestViewState extends State<FirstTestView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
